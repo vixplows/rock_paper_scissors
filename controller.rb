a1f4106b8402e9de3046e0fa43dbae2856fd8c0f
vixplows <vicky@vickyplows.co.uk>
2017-06-12 22:28:05 +0100

    Add css file and formatting

--- controller.rb
+++ controller.rb
@@ -3,21 +3,20 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( './models/game' )
 
+get '/' do
+  erb(:home)
+end
+
+get '/about_us' do
+  erb(:about_us)
+end
+
+get '/play' do
+  erb(:play)
+end
 
 get '/play/:choice1/:choice2' do
   game = Game.new(params[:choice1], params[:choice2])
   @winner = game.play()
-  erb (:result)
+  erb(:result)
 end
-
-# get '/play/rock/scissor' do
-#  return "rock beats scissor"
-# end
-
-# get '/play/paper/rock' do
-#   return "paper beats rock"
-# end
-
-# get '/play/scissor/paper' do
-#   return "scissors beats paper"
-# end
